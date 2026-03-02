efbc7a3ad32fabeecb597405d98254f1326e189c
55DevDanilo <dev.danilo_asl@outlook.com>
2026-03-02 19:30:39 -0300

    Entidade Pedido. Datas com Instant e padrão ISO 8601 - PARTE 1

--- course/src/main/java/com/educandoweb/course/entities/Order.java
+++ course/src/main/java/com/educandoweb/course/entities/Order.java
@@ -1,10 +1,30 @@
 package com.educandoweb.course.entities;
 
+import java.io.Serializable;
 import java.time.Instant;
+import java.util.Objects;
 
-public class Order {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="tb_order")
+public class Order implements Serializable{ //A serialização é a conversão do estado de um objeto em um fluxo de bytes para que esse objeto possa ser trafegado pela rede http, {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private Instant moment;
+	
+	@ManyToOne
+	@JoinColumn(name = "client_id")
 	private User client;
 	public Order() {
 		
@@ -32,6 +52,21 @@ public class Order {
 	}
 	public void setClient(User client) {
 		this.client = client;
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Order other = (Order) obj;
+		return Objects.equals(id, other.id);
 	}
 	
 	
