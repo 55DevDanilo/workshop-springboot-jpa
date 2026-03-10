1533eab015a05655379bc63b393c06949960dfc9
55DevDanilo <dev.danilo_asl@outlook.com>
2026-03-09 22:33:55 -0300

    Associação muitos-para-muitos com JoinTable

--- course/src/main/java/com/educandoweb/course/entities/Category.java
+++ course/src/main/java/com/educandoweb/course/entities/Category.java
@@ -5,28 +5,34 @@ import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import jakarta.persistence.Transient;
 
 @Entity
-@Table(name="tb_category")
-public class Category implements Serializable{
-	
-	
+@Table(name = "tb_category")
+public class Category implements Serializable {
+
 	private static final long serialVersionUID = 1L;
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
-	@Transient
+
+	@JsonIgnore
+	@ManyToMany(mappedBy = "categories")
 	private Set<Product> products = new HashSet<>();
-	
-	Category(){
-		
+
+	Category() {
+
 	}
 
 	public Category(Long id, String name) {
@@ -47,12 +53,10 @@ public class Category implements Serializable{
 		return name;
 	}
 
-	
-
 	public void setName(String name) {
 		this.name = name;
 	}
-	
+
 	public Set<Product> getProducts() {
 		return products;
 	}
@@ -73,7 +77,5 @@ public class Category implements Serializable{
 		Category other = (Category) obj;
 		return Objects.equals(id, other.id);
 	}
-	
-	
 
 }
